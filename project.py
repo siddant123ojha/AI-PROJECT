6acdbbcda93b477c2b58deb3fa2fa94392221413
Siddant Ojha <122614612+siddant123ojha@users.noreply.github.com>
2025-11-12 09:39:08 +0000

    project requirements secrets

--- project.py
+++ project.py
@@ -2,6 +2,7 @@ from google import genai
 import time
 from PIL import Image
 from io import BytesIO
+from diffusers import AutoPipelineForText2Image
 import sys
 import streamlit
 from google.genai import types
@@ -14,24 +15,29 @@ def generative_teaching_ai():
     response=client.models.generate_content(
         model="gemini-2.5-flash",
         contents=prompt_teach_input,
-        config=types.GenerateContent Config(temperature=0.1,max_output_tokens=max_outtokens)
+        config=types.GenerateContentConfig(temperature=0.1,max_output_tokens=max_outtokens)
     )
     print(response.text)
   
 def generative_math_ai():
-    while True:
         prompt_teach_input=input("Enter a math question: ")
         mathresponse=client.models.generate_content(
             model="gemini-2.5-flash",
-            contents="Explain with detail"+prompt_teach_input,
+            contents="You are a math ai solver, and do not answer any thing else also if i ask something as easy as 1+1 dont explain it in full unneccesary detail but i ask something like xcos45+xsin45=1 explain it and answer this equation: "+prompt_teach_input,
             config=types.GenerateContentConfig(temperature=0.1,max_output_tokens=max_outtokens)
     )
         print(mathresponse.text)
 
-choose=input("Enter which model do you want to use:\n1.Ai teaching assistant\n2.Math Ai\n")
-if choose=="1":
-    generative_teaching_ai()
-elif choose=="2":
-    generative_math_ai()
-else:
-    print("please chooes one or 2")
+
+def main():
+    while True:
+        choose=input("Enter which model do you want to use:\n1.Ai teaching assistant\n2.Math Ai\n")
+        if choose=="1":
+            generative_teaching_ai()
+        elif choose=="2":
+            generative_math_ai()
+        else:
+            print("please choose 1,2")
+
+if __name__=="__main__":
+    main()
