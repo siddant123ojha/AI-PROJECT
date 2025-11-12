302c74d0977a198624c80532ad8dd526b1783b2a
Siddant Ojha <122614612+siddant123ojha@users.noreply.github.com>
2025-11-12 06:02:03 +0000

    project

--- project.py
+++ project.py
@@ -2,23 +2,17 @@ import google.genai
 import time
 import PIL
 import sys
-from secrets import image_apikey
-from secrets import math_apikey
-from secrets import teach_apikey
 import streamlit
-image_key=google.genai.Client(image_apikey)
-math_apikey=google.genai.Client(math_apikey)
-teach_apikey=google.genai.Client(teach_apikey)
-
+from google.genai import types
+client = google.client()
 max_char=10000
-
+aimodels=google.genai.models("gemini-2.5-flash")
+imagemodels=google.genai.models("gemini-2.5-flash-image")
+mathmodels=google.genai.models("gemini-2.5-flash")
 def generative_teaching_ai():
-    google.genai.models("gemini-2.5-flash")
-    
-def run_ai_teaching_assistant():
-    streamlit.title("📖Ai teaching assistant")
-    streamlit.container(border=True, width="stretch",height="content")
-    streamlit.chat_input("Enter your message", max_char=max_char,accept_file=False)
-    streamlit.button("Send")
-    
-run_ai_teaching_assistant()
+    prompt_teach_input=streamlit.chat_input("Enter a prompt")
+    response=client.models.generate.text(
+        model=aimodels,
+        prompt=prompt_teach_input,
+        config=types.GenerateContentConfig()
+    )
